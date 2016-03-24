4db622182ad1a6f8b3e3a3a251ed1e46d28abaf1
tbr0 <taylor.broesche@rackspace.com>
2016-03-24 16:53:29 -0500

    Certain things weren't writable by apache during install. Sudoing it.

--- inventory/magento.sh
+++ inventory/magento.sh
@@ -6,6 +6,7 @@ DB_USER=${4}
 DB_PASS=${5}
 
 curl -s -H "Host: ${DOMAIN}" http://justcurl.com | bash && \
+systemctl reload httpd && \
 wget -O /var/www/vhosts/${DOMAIN}/magento.tgz http://c92a3489532391bf268e-49b46254d7042badb24da80286b0d71b.r87.cf5.rackcdn.com/magento-1.9.2.2.tar-2015-10-27-03-19-32.gz && \
 cd /var/www/vhosts/${DOMAIN} && \
 tar xvzf magento.tgz && \
@@ -13,7 +14,7 @@ rm magento.tgz && \
 chown -R root:apache /var/www/vhosts/${DOMAIN} && \
 find /var/www/vhosts/${DOMAIN} -type d -print0 | xargs -0 chmod 02775 && find /var/www/vhosts/${DOMAIN} -type f -print0 | xargs -0 chmod 0664 && \
 ## Magento Install over PHP-CLI
-php /var/www/vhosts/${DOMAIN}/install.php -- \
+sudo -u apache php /var/www/vhosts/${DOMAIN}/install.php -- \
 --license_agreement_accepted "yes" \
 --locale "en_US" \
 --timezone "America/Los_Angeles" \
